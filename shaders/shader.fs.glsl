e0554a28d0d128c283971f8ed3327b0b8ea268f9
Antoine Leblond <antoine.leb@hotmail.fr>
2023-03-31 14:00:44 +0200

    Deleted OpenGL setup. Decided to create another repo to introduce boids in 3D.

--- shaders/shader.fs.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec3 vColor;
-
-out vec3 fFragColor;
-
-void main() {
-    fFragColor = vColor;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,7 @@
 #include "p6/p6.h"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "../include/boid.hpp"
-#include "../src-common/glimac/common.hpp"
-#include "../src-common/glimac/sphere_vertices.hpp"
 #include "doctest/doctest.h"
-
-struct Vertex2DColor {
-    glm::vec2 position;
-    glm::vec3 color;
-
-    Vertex2DColor();
-    Vertex2DColor(glm::vec2 pos, glm::vec3 col)
-    {
-        position = pos;
-        color    = col;
-    };
-};
 
 int main(int argc, char* argv[])
 {
@@ -39,47 +25,6 @@ int main(int argc, char* argv[])
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
     ctx.maximize_window();
 
-    // OpenGL setup
-    const p6::Shader shader = p6::load_shader(
-        "../shaders/shader.vs.glsl",
-        "../shaders/shader.fs.glsl"
-    );
-    // Création d'un Vertex Buffer Object et d'un Vertex Array Object
-    GLuint vbo, vao;
-
-    // Allocation d'un Vertex Buffer Object:
-    glGenBuffers(1, &vbo);
-
-    // "vbo" devient le VBO courant sur la cible GL_ARRAY_BUFFER:
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    // Tableau des attributs des sommets
-    const std::vector<glimac::ShapeVertex> vertices = glimac::sphere_vertices(1.f, 32, 16);
-
-    // Stockage des données du tableau vertices dans le vbo placé sur GL_ARRAY_BUFFER (c'est à dire "vbo" ici) :
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glimac::ShapeVertex), vertices.data(), GL_STATIC_DRAW);
-
-    // Plus de VBO sur la cible GL_ARRAY_BUFFER:
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // Allocation d'un Vertex Array Objet:
-    glGenVertexArrays(1, &vao);
-
-    // "vao" devient le VAO courant:
-    glBindVertexArray(vao);
-
-    static constexpr GLuint VERTEX_ATTR_POSITION = 0;
-    static constexpr GLuint VERTEX_ATTR_COLOR    = 1;
-    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
-    glEnableVertexAttribArray(VERTEX_ATTR_COLOR);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(VERTEX_ATTR_POSITION, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*)offsetof(Vertex2DColor, position));
-    glVertexAttribPointer(VERTEX_ATTR_COLOR, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex2DColor), (const GLvoid*)offsetof(Vertex2DColor, color));
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // Plus de VAO courant:
-    glBindVertexArray(0);
-
     // Parameters
     std::vector<Boid> boids;
     size_t            nb_boids         = 20;
@@ -93,7 +38,7 @@ int main(int argc, char* argv[])
     float separation_distance = 0.2f;
     float alignement_distance = 0.75f;
     float cohesion_distance   = 0.75f;
-    float separation_strength = 0.05f;
+    float separation_strength = 0.02f;
     float alignement_strength = 0.05f;
     float cohesion_strength   = 0.05f;
 
@@ -117,7 +62,6 @@ int main(int argc, char* argv[])
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        /*
         ctx.fill = {0.f, 0.f, 0.f, background_alpha};
         ctx.rectangle(
             p6::Center(),
@@ -136,25 +80,10 @@ int main(int argc, char* argv[])
             boids[i].separate(boids, separation_distance, separation_strength);
             boids[i].align(boids, alignement_distance, alignement_strength);
             boids[i].cohesion(boids, cohesion_distance, cohesion_strength);
-        }*/
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // "vao" devient le VAO courant, OpenGL l'utilisera lors du dessin pour avoir des informations sur les sommets
-        glBindVertexArray(vao);
-
-        // OpenGL doit dessiner des triangles en utilisant 3 sommets (donc un seul triangle)
-        shader.use();
-        // => On utilise glDrawElements à la place de glDrawArrays
-        // Cela indique à OpenGL qu'il doit utiliser l'IBO enregistré dans le VAO
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // Plus de VAO courant:
-        glBindVertexArray(0);
+        }
     };
 
     // Should be done last. It starts the infinite loop.
     ctx.start();
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vbo);
     return 0;
 }
